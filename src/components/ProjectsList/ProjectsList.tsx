11e5eca2cc754980b09072d85141c096acdcad36
Kumani K <kumanikidd@gmail.com>
2023-12-28 15:02:58 +0000

    project list html complete

--- src/components/ProjectsList/ProjectsList.tsx
+++ src/components/ProjectsList/ProjectsList.tsx
@@ -1,10 +1,15 @@
 import ProjectCard from "../ProjectCard/ProjectCard";
+import Projects from "../../data/Projects";
 
 const ProjectsList = () => {
     return (
         <div>
         <h1>ProjectsList</h1>
-        <ProjectCard />
+            <div className="projects-list__card-container">
+                {Projects.map((project) => {
+                    return <ProjectCard project={project} />;
+                })}
+            </div>
         </div>
     );
 }
