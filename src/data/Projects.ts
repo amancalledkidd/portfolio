491c919992a3adb3e8286a4de27154a7b9aeb8da
Kumani K <kumanikidd@gmail.com>
2023-12-28 15:00:54 +0000

    Mock projects added

---
+++ src/data/Projects.ts
@@ -0,0 +1,38 @@
+import Project from "../types/Project";
+
+const Projects: Project[] = [
+    {
+        id: 1,
+        name: "Project 1",
+        description: "This is the first project.",
+        image: "https://via.placeholder.com/150",
+        github_url: "https://github.com",
+        live_url: "https://github.com",
+    },
+    {
+        id: 2,
+        name: "Project 2",
+        description: "This is the second project.",
+        image: "https://via.placeholder.com/150",
+        github_url: "https://github.com",
+        live_url: "https://github.com",
+    },
+    {
+        id: 3,
+        name: "Project 3",
+        description: "This is the third project.",
+        image: "https://via.placeholder.com/150",
+        github_url: "https://github.com",
+        live_url: "https://github.com",
+    },
+    {
+        id: 4,
+        name: "Project 4",
+        description: "This is the fourth project.",
+        image: "https://via.placeholder.com/150",
+        github_url: "https://github.com",
+        live_url: "https://github.com",
+    },
+];
+
+export default Projects;
