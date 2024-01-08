8b06d678674a319cafe644c9dd34fe95832ec63c
Kumani K <kumanikidd@gmail.com>
2024-01-08 13:20:06 +0000

    text adjustment

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -6,7 +6,7 @@ const Intro = () => {
             <div className="intro__content">
                 <h1 className="intro__content__title">Kumani Kidd - Junior Software Engineer</h1>
                 <h2 className="intro__content__subtitle"></h2>
-                <p className="intro__content__description">I'm a software engineer based in London, UK specializing in building (and occasionally designing) exceptional, high-quality websites and applications.</p>
+                <p className="intro__content__description">I'm a fullstack Junior Software Engineer based in London, UK. Proficient in Javascript and Python. </p>
             </div>
         </div>
     );
--- src/data/Me.ts
+++ src/data/Me.ts
@@ -4,7 +4,7 @@ import profilePic from '../assets/images/profile-pic.jpeg';
 
 const Me: Developer = {
     name: "Kumani Kidd",
-    description: "Hi, I'm Kumani, a passionate Junior Software Engineer with a deep-rooted love for technology. My journey began in the world of video technology, where I developed strong problem-solving skills and honed my teamwork abilities. Driven by an ever-growing fascination with software engineering, I took a bold step, transitioning from manufacturing to a software development bootcamp. Now, I'm excited to delve deeper into this field with _nology, expanding my expertise in Python, JavaScript, React, and other cutting-edge technologies. My commitment to continuous learning and my diverse skill set make me ready to take on new challenges.",
+    description: "Hi, I'm Kumani, a passionate Junior Software Engineer with a deep-rooted love for technology. My journey began in the world of video technology, where I developed strong problem-solving skills and honed my teamwork abilities. Driven by an ever-growing fascination with software engineering, I took a bold step, transitioning from manufacturing to a software development bootcamp. Now, I'm excited to delve deeper into this field with _nology, expanding my expertise in TypeScript, React, and other cutting-edge technologies. My commitment to continuous learning and my diverse skill set make me ready to take on new challenges.",
     image: profilePic,
     role: "Junior Software Engineer",
     location: "London, UK",
